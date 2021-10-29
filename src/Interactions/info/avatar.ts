3b2a6cdbad25559acfe6298bc29ea694e8c42fa1
hyduez <hyduez@outlook.es>
2021-10-29 09:35:52 -0500

    Spiderman

--- src/Interactions/info/avatar.ts
+++ src/Interactions/info/avatar.ts
@@ -6,6 +6,7 @@ export const interaction = {
     type: 2,
     run: async (bot: Client, interact: CommandInteraction, _args: string[]) => {
         const user = await bot.users.get(interact.data.target_id);
-        await interact.createMessage({ content: user.dynamicAvatarURL('gif' || 'png', 1024), flags: 64 });
+        const dynamic = user.avatar ? (user.avatar.startsWith('a_') ? 'gif' : 'png') : 'png';
+        await interact.createMessage({ content: user.dynamicAvatarURL(dynamic, 1024), flags: 64 });
     }
 }
